00a56d2d667fde60763d01e4bf6cfc36b5c05730
JaiderValencia <janvame05@gmail.com>
2023-05-06 20:09:35 -0500

    add EJS

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const path = require('path');
 
 const app = express();
 
+//statics
 app.use(express.static(path.resolve(__dirname, './public')));
+
+//EJS
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, './views'))
 
 app.listen(3000);
