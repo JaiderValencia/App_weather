bc1c4eec4006a4bfce8d44297bc066766c4ed8da
JaiderValencia <janvame05@gmail.com>
2023-05-07 14:58:32 -0500

    add dinamyc colors class for weather by back-end

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,10 +2,41 @@ const fetch = require('node-fetch');
 
 const controller = {
     index: async (req, res) => {
-        const result = await fetch('https://api.weatherapi.com/v1/current.json?key=c4f2cbb0ab214c09afd40442230605&q=5.696108,-76.658462')
+        const result = await fetch('https://api.weatherapi.com/v1/current.json?key=c4f2cbb0ab214c09afd40442230605&q=medellin')
             .then(response => response.json());
 
-        res.render('index', { result })
+        let weatherClass;
+        let weatherRoundedClass;
+
+        const temps = {
+            heat: {
+                between: {
+                    min: 27,
+                    max: Infinity
+                }
+            },
+            cool: {
+                between: {
+                    min: 8,
+                    max: 26
+                }
+            },
+            cold: {
+                between: {
+                    min: -Infinity,
+                    max: 10
+                }
+            }
+        }
+
+        for (const key in temps) {
+            if (result.current.temp_c >= temps[key].between.min && result.current.temp_c <= temps[key].between.max) {
+                weatherClass = `${key}-background-color`;
+                weatherRoundedClass = `${key}-rounded-background`;
+            }
+        }
+
+        res.render('index', { result, weatherClass, weatherRoundedClass });
     }
 };
 
