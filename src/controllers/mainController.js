f39ecade5fff3a197ed1bbff8fe2ed9a0a45d7b8
JaiderValencia <janvame05@gmail.com>
2023-05-06 23:05:47 -0500

    add dinamyc data by API

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,7 +2,10 @@ const fetch = require('node-fetch');
 
 const controller = {
     index: async (req, res) => {
-        res.render('index.ejs');
+        const result = await fetch('https://api.weatherapi.com/v1/current.json?key=c4f2cbb0ab214c09afd40442230605&q=5.696108,-76.658462')
+            .then(response => response.json());
+
+        res.render('index', { result })
     }
 };
 
