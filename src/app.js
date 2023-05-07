eb19a340352c919f349527ab4719cc5bb858ac6b
JaiderValencia <janvame05@gmail.com>
2023-05-06 20:07:10 -0500

    Agrego los archivos publicos

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.listen(3000)
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(3000);
