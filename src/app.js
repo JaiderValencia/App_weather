e4898dd4a54f44c1f3014d4b8224a129acf45e0f
JaiderValencia <janvame05@gmail.com>
2023-05-06 20:22:37 -0500

    add first view

--- src/app.js
+++ src/app.js
@@ -3,11 +3,17 @@ const path = require('path');
 
 const app = express();
 
+//routes require
+const mainRoutes = require('./routes/mainRoutes');
+
 //statics
 app.use(express.static(path.resolve(__dirname, './public')));
 
 //EJS
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, './views'))
+
+//use routes
+app.use('/', mainRoutes)
 
 app.listen(3000);
---
+++ src/controllers/mainController.js
@@ -0,0 +1,7 @@
+const controller = {
+    index: async (req, res) => {
+        res.render('index.ejs');
+    }
+};
+
+module.exports = controller;
